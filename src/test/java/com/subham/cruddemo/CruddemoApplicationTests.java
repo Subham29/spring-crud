5b3f5af26565ee5ab77ec473f7a2e0f103405096
Subham <subham.patel@temenos.com>
2023-04-11 23:53:07 +0530

    Initial Commit for Spring Crud Demo

---
+++ src/test/java/com/subham/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subham.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
