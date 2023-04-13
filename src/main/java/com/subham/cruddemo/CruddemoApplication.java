f8bb160bedb07c41ac3190403bcc8a84cd1cc454
Subham <subham.patel@temenos.com>
2023-04-14 00:57:38 +0530

    Added findAll functionality to print all the students from the Student Table with Sorting

--- src/main/java/com/subham/cruddemo/CruddemoApplication.java
+++ src/main/java/com/subham/cruddemo/CruddemoApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CruddemoApplication {
 
@@ -19,8 +21,17 @@ public class CruddemoApplication {
 		return runner -> {
 //			createStudent(studentDAO);
 //			createMultipleStudents(studentDAO);
-			readStudent(studentDAO);
+//			readStudent(studentDAO);
+			queryForStudents(studentDAO);
 		};
+	}
+
+	private void queryForStudents(StudentDAO studentDAO) {
+		System.out.println("Listing all students");
+		List<Student> allStudents = studentDAO.findAll();
+		for (Student student : allStudents) {
+			System.out.println(student);
+		}
 	}
 
 	private void readStudent(StudentDAO studentDAO) {
--- src/main/java/com/subham/cruddemo/dao/api/StudentDAO.java
+++ src/main/java/com/subham/cruddemo/dao/api/StudentDAO.java
@@ -2,8 +2,12 @@ package com.subham.cruddemo.dao.api;
 
 import com.subham.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
     void save(Student student);
 
     Student findById(int id);
+
+    List<Student> findAll();
 }
